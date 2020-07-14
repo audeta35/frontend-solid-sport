9bf0d795a8c53bf2ec55159445b627e9921f8d3d
audeta35 <audeta35@gmail.com>
2020-07-14 18:48:50 +0700

    dev#37: Grouping form process

--- src/app/components/add-atlet/add-atlet.component.ts
+++ src/app/components/add-atlet/add-atlet.component.ts
@@ -6,38 +6,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-atlet.component.css']
 })
 export class AddAtletComponent implements OnInit {
-  names: any;
+
+  group: any = [];
+  maxUserGroup: number = 10;
+  names: any = [];
+  totalForm: number;
   selectedAll: any;
+  angka: number = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.names = [
-      { name: 'Prashobh', selected: false },
-      { name: 'Abraham', selected: false },
-      { name: 'Anil', selected: false },
-      { name: 'Sam', selected: false },
-      { name: 'Natasha', selected: false },
-      { name: 'Marry', selected: false },
-      { name: 'Zian', selected: false },
-      { name: 'karan', selected: false },
-      { name: 'Marry', selected: false },
-      { name: 'Zian', selected: false },
-      { name: 'karan', selected: false },
-      { name: 'karan', selected: false },
-    ]
-  }
+  ngOnInit(): void {}
+
   selectAll() {
     for (var i = 0; i < this.names.length; i++) {
       this.names[i].selected = this.selectedAll;
       console.log(this.names)
     }
   }
+
   checkIfAllSelected() {
     this.selectedAll = this.names.every(function (item: any) {
       return item.selected == true;
-
     })
+  }
+
+  setForm() {
+
+    let total = this.totalForm / this.maxUserGroup;
+    let result = Math.ceil(total);
+    let i: number;
+
+    // set group
+
+    for(i=1; i<=result; i++) {
+      this.group.push({
+        name : `group ${i}`,
+        value : 0
+      })
+    }
+
+    for(i=1; i<=result; i++) {
+      for (let x = 1; x <= this.maxUserGroup; x++) {
+
+        this.names.push({
+          name: "",
+          selected: false,
+          group: `group ${i}`
+        })
+      }
+    }
+
+    for(i=0; i<=this.names.length; i++) {
+      if (i < this.totalForm) {
+        this.names.splice(this.totalForm, i);
+      }
+    }
+
+    console.log(this.names)
+  }
+
+  addForm(group) {
+
+    this.names.push({
+      name: "",
+      group: group
+    })
+  }
+
+  deleteForm() {
+    let i;
+    let length: any = this.names.length
+    for(i = 0; i < length; i++ ) {
+      if(this.names[i].selected) {
+        this.names.splice(i, 1); i--;
+      }
+    }
+
+    console.log("names : ", this.names);
+    console.log("group :", this.group);
+
   }
 
   numberOnly(event): boolean {
@@ -46,6 +94,10 @@ export class AddAtletComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  submitForm() {
+    console.table(this.names);
   }
 
 }
