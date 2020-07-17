3203dc3c84fb679328c91d6531802d5679abe5e4
audeta35 <audeta35@gmail.com>
2020-07-17 18:40:52 +0700

    Master : add match for athlete

--- src/app/components/file-atlet/file-atlet.component.ts
+++ src/app/components/file-atlet/file-atlet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AtletService } from 'src/app/provider/services/atlet';
+import { MatchService } from 'src/app/provider/services/match';
 
 @Component({
   selector: 'app-file-atlet',
@@ -10,16 +11,49 @@ export class FileAtletComponent implements OnInit {
 
   atlet:any = [];
   group:any = [];
+  match:any = [];
+  totalMatch:any;
 
   constructor(
-    private atletService: AtletService
+    private atletService: AtletService,
+    private matchService: MatchService
   ) { }
 
   ngOnInit(): void {
+    this.getAllAtlet();
+  }
+
+  getAllAtlet() {
     this.atletService.getAtlet()
+      .then((res: any) => {
+        this.atlet = res.result.atlet;
+        this.group = res.result.group;
+        this.totalMatch = Object.keys(res.result.atlet);
+        this.getAllMatch();
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  getAllMatch() {
+    this.matchService.getMatch()
     .then((res:any) => {
-      this.atlet = res.result.atlet;
-      this.group = res.result.group;
+      this.match = res.result;
+      this.isMatch();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  isMatch() {
+    console.log(this.totalMatch)
+  }
+
+  startGroupMatch(atlet) {
+    this.matchService.addGroupMatch(atlet)
+    .then((res) => {
       console.log(res)
     })
     .catch((err) => {
