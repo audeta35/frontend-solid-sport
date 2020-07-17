00e3259cc7f01e815d5edb807882a34c580b4185
luull <m.fadlullah14@gmail.com>
2020-07-17 16:12:19 +0700

    dev#49:added new page recap

---
+++ src/app/components/recap/recap.component.routing.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecapComponent } from './recap.component';
+
+const routes: Routes = [
+    { path: '', component: RecapComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forChild(routes);
