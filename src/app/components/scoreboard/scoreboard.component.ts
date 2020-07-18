310661f300a007ab21c13929c7e6e481b6b6f6ba
audeta35 <audeta35@gmail.com>
2020-07-18 14:05:56 +0700

    assessment: realtime for scoreboard

--- src/app/components/scoreboard/scoreboard.component.ts
+++ src/app/components/scoreboard/scoreboard.component.ts
@@ -26,89 +26,85 @@ export class ScoreboardComponent implements OnInit {
 
   constructor(private socket: Socket, private atletService: AtletService, private pointService: PointService) {
     socket.emit('scoreboard');
+    socket.on('data-score', () => {
+      this.atletService.getAtletByMatch()
+        .then(res => {
+          console.log(res)
+          this.userData = res['result'][0];
+          console.log('========', this.userData);
+          this.pointService.getPointForScoreboard(this.userData.id_atlet, this.userData.id_match)
+            .then(response => {
+              console.log('=== response', response)
+              this.pointList = response['result']['athlete_point_list'];
+              this.finalScore = response['result']['total_point'];
+
+              for (let i = this.pointList.length; i < 10; i++) {
+                console.log(i)
+                if (i === 7) {
+                  this.pointList.push({
+                    FAC_ATH: 0.3,
+                    FAC_TECH: 0.7,
+                    noColor: true
+                  })
+                } else if (i < 7) {
+
+                  this.pointList.push({
+                    technicalValue: 0,
+                    athleticValue: 0
+                  })
+                } else if (i > 7 && i < 9) {
+                  this.pointList.push({
+                    technical_point_result: response['result']['technical_point_result'],
+                    athletic_point_result: response['result']['athletic_point_result'],
+                    noColor: true,
+                  })
+                } else {
+                  this.pointList.push({
+
+                    technical_point: response['result']['technical_point'],
+                    athletic_point: response['result']['athletic_point'],
+                    noColor: true
+                  })
+                }
+              }
+              console.log(this.pointList)
+              this.isLoading = false;
+            })
+            .catch(error => {
+
+              console.log('=== error gan', error)
+              if (error['status'] === 404) {
+                for (let i = 0; i < 10; i++) {
+                  if (i == 7) {
+                    this.pointList.push({
+                      FAC_ATH: 0.3,
+                      FAC_TECH: 0.7
+                    })
+                  } else {
+
+                    this.pointList.push({
+                      technicalValue: 0,
+                      athleticValue: 0
+                    })
+                  }
+                }
+              }
+
+              this.isLoading = false;
+            })
+        })
+        .catch(err => {
+          console.log('====error gan', err);
+          if (err['status'] === 404) {
+            console.log('notFound');
+          }
+          this.isLoading = false;
+        })
+    })
   }
 
   ngOnInit(): void {
-    // this.socket.on('data-score', (data) => {
-    //   this.atlet = data;
-    //   console.log(this.atlet);
-    // });
-
     this.isLoading = true;
-
-    this.atletService.getAtletByMatch()
-    .then(res => {
-      console.log(res)
-      this.userData = res['result'][0];
-      console.log('========',this.userData);
-      this.pointService.getPointForScoreboard(this.userData.id_atlet, this.userData.id_match)
-      .then(response => {
-        console.log('=== response', response)
-        this.pointList = response['result']['athlete_point_list'];
-        this.finalScore = response['result']['total_point'];
-
-        for(let i = this.pointList.length; i < 10; i++) {
-          console.log(i)
-          if(i === 7) {
-            this.pointList.push({
-              FAC_ATH: 0.3,
-              FAC_TECH: 0.7,
-              noColor: true
-            })
-          } else if(i < 7){
-            
-            this.pointList.push({
-              technicalValue: 0,
-              athleticValue: 0
-            })
-          } else if(i > 7 && i < 9) {
-            this.pointList.push({
-              technical_point_result: response['result']['technical_point_result'],
-              athletic_point_result: response['result']['athletic_point_result'],
-              noColor: true,
-            })
-          } else {
-            this.pointList.push({
-
-              technical_point: response['result']['technical_point'],
-              athletic_point: response['result']['athletic_point'],
-              noColor: true
-            })
-          }
-        }
-        console.log(this.pointList)
-        this.isLoading = false;
-      })
-      .catch(error => {
-
-        console.log('=== error gan', error)
-        if(error['status'] === 404) {
-          for(let i = 0; i < 10; i++) {
-            if(i == 7) {
-              this.pointList.push({
-                FAC_ATH: 0.3,
-                FAC_TECH: 0.7
-              })
-            } else {
-              
-              this.pointList.push({
-                technicalValue: 0,
-                athleticValue: 0
-              })
-            }
-          }
-        }
-
-        this.isLoading = false;
-      })
-    })
-    .catch(err => {
-      console.log('====error gan', err);
-      if(err['status'] === 404) {
-        console.log('notFound');
-      }
-      this.isLoading = false;
-    })
 
   }
 }
