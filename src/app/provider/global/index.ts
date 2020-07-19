8bd3a805f247646a98dccea18917c053772d084c
audeta35 <audeta35@gmail.com>
2020-07-19 14:35:07 +0700

    http client: set https

--- src/app/provider/global/index.ts
+++ src/app/provider/global/index.ts
@@ -16,7 +16,7 @@ export class GlobalProvider {
   config(url: string) {
     // let path = "http://localhost:3000/";
     // let path = 'http://192.168.10.116:3000/';
-    let path = "http://solidapi.bisapastibisa.com/";
+    let path = "https://solidapi.bisapastibisa.com/";
     const config = {
       "juriUrl" : path + "users/",
       "atletUrl" : path + "atlet/",
