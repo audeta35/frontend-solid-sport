f3cc84d7f230196ef4dea36e684a0057dfca5d99
audeta35 <audeta35@gmail.com>
2020-07-21 01:08:20 +0700

    tatami: add tatami and class `configuration`

--- src/app/components/index/index.component.module.ts
+++ src/app/components/index/index.component.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IndexComponent } from './index.component';
 import { routing } from './index.component.routing';
 
@@ -10,7 +10,7 @@ const path = "http://localhost:3000/";
 const config: SocketIoConfig = { url: path, options: {} };
 
 @NgModule({
-  imports: [routing, HttpClientModule, CommonModule, ReactiveFormsModule, SocketIoModule.forRoot(config)],
+  imports: [routing, HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule, SocketIoModule.forRoot(config)],
     declarations: [IndexComponent]
 })
 export class IndexComponentModule { }
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { TatamiServices } from 'src/app/provider/services/tatami';
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2000,
+});
 
 @Component({
   selector: 'app-index',
@@ -9,12 +18,79 @@ import { Router } from '@angular/router';
 })
 export class IndexComponent implements OnInit {
 
-  constructor(private routes: Router) {
+  tatami:any = [];
+  listTatami:number = 0;
+  config:boolean = false;
+  isLoading:boolean = false;
 
-  }
+  constructor(private routes: Router, private tatamiService: TatamiServices) {}
 
   ngOnInit(): void {
+    this.getTatami()
+  }
 
+  getTatami() {
+    this.tatamiService.getTatami()
+    .then((res:any) => {
+      this.listTatami = res.result.length;
+      console.log(res.result.length)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  configBtn() {
+    this.config = !this.config;
+  }
+
+  alert() {
+    Toast.fire({
+      icon: 'info',
+      title: `Tidak dapat di proses, harap lengkapi data`,
+    });
+  }
+
+  addForm() {
+    this.tatami.push({
+      name: ``,
+      class: ``
+    })
+  }
+
+  delForm(id) {
+    this.tatami.splice(id, 1)
+  }
+
+  submitForm() {
+    this.isLoading = !this.isLoading;
+
+    let payload = {
+      payload : this.tatami
+    }
+
+    setTimeout(() => {
+      this.tatamiService.insertTatami(payload)
+      .then((res) => {
+        Toast.fire({
+          icon: 'success',
+          title: `Konfigurasi berhasil`,
+        });
+        this.getTatami();
+        this.tatami = [];
+        this.isLoading = !this.isLoading;
+      })
+      .catch((err) => {
+        Toast.fire({
+          icon: 'error',
+          title: `Kesalahan pada server`,
+        });
+        console.log(err)
+        this.getTatami();
+        this.tatami = [];
+        this.isLoading = !this.isLoading;
+      })
+    }, 500)
   }
 
 }
