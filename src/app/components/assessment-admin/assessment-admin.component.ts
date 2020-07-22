8c180d06ee089934fb7232c66259d6c3b791945c
maslow123 <mfadhlyn@gmail.com>
2020-07-22 22:22:57 +0700

    dev#69: Connect display athlete assessment to API

--- src/app/components/assessment-admin/assessment-admin.component.ts
+++ src/app/components/assessment-admin/assessment-admin.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { AtletService } from 'src/app/provider/services/atlet';
 import { PointService } from 'src/app/provider/services/points';
+import Swal from 'sweetalert2';
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2000,
+});
 @Component({
   selector: 'app-assessment-admin',
   templateUrl: './assessment-admin.component.html',
@@ -160,5 +167,60 @@ export class AssessmentAdminComponent implements OnInit {
     this.socket.emit('scoreboard');
     this.socket.emit('result-admin');
     this.socket.emit('result-juri');
+  }
+
+  athleteReset() {
+    console.log('masuk', this.userData)
+    if(this.userData) {
+      let counter = 1;
+      for(let i in this.optionValue) {
+        for(let j in this.optionValue[i].data) {
+          if(this.optionValue[i].data[j].selectedTechnical) {
+            counter++;
+          }
+        }
+      }
+      if(counter === 7) {
+        this.pointService.changeAthleteAssessment(this.userData.id_atlet)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+          if(err['status'] === 404) {
+            this.isLoading = true;
+            console.log('masuk gag');
+            this.optionValue = [];
+            this.userData = {};
+            for (let i = 0; i < 7; i++) {
+              this.optionValue.push({
+                userId: i + 1,
+                data: []
+              });
+  
+              for (let j = 5.0; j < 10; j = j + 0.2) {
+                let point = j.toFixed(1);
+                this.optionValue[i].data.push({ point: point });
+              }
+            }
+          }
+  
+          this.isLoading = false;
+          console.log(this.optionValue)
+        })
+      } else {
+        Toast.fire({
+          icon: 'info',
+          text:
+            'Mohon lengkapi penilaian atlet terlebih dahulu',
+        });
+      }
+    } else {
+      Toast.fire({
+        icon: 'info',
+        text:
+          'Mohon lengkapi penilaian atlet terlebih dahulu',
+      });
+    }
   }
 }
